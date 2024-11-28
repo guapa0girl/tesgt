6bdd760f6ac5caa35bc164c53d70b0c78a78d05d
guapa0girl <guapapamama@gmail.com>
2024-11-28 21:29:55 +0900

    api 연동

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "api/", // 서버 주소
-    withCredentials: true, // 쿠키 전송을 위해 설정
+  baseURL: "https://2fe1-203-246-85-188.ngrok-free.app/api/", // 서버 주소
+  withCredentials: true, // 쿠키 전송을 위해 설정
 });
 
 export default axiosInstance;
